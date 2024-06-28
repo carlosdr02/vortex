6ba0bd1ddda249aec0eb52fab8182bf534c62671
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-06-28 16:52:09 +0200

    Added the main menu bar to the GUI

--- src/application/gui.cpp
+++ src/application/gui.cpp
@@ -3,12 +3,24 @@
 #include <imgui_impl_vulkan.h>
 #include <imgui_impl_glfw.h>
 
+using namespace ImGui;
+
+static void renderMainMenuBar() {
+    if (BeginMainMenuBar()) {
+        if (BeginMenu("File")) {
+            EndMenu();
+        }
+
+        EndMainMenuBar();
+    }
+}
+
 void renderGui() {
     ImGui_ImplVulkan_NewFrame();
     ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+    NewFrame();
 
-    ImGui::ShowDemoWindow();
+    renderMainMenuBar();
 
-    ImGui::Render();
+    Render();
 }
