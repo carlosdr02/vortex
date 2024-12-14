d7cc3e55abe82c2a8bed17c9c0db9634ff10a577
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-12-14 23:10:54 +0100

    Added the Images directory creation to the Project class

--- src/application/project.cpp
+++ src/application/project.cpp
@@ -5,7 +5,9 @@
 Project::Project(const std::filesystem::path& path) : path(path) {
     std::filesystem::create_directory(path);
     std::ofstream projectFile(path / "project.vx");
-    std::filesystem::create_directory(getContentPath());
+    std::filesystem::path contentPath = getContentPath();
+    std::filesystem::create_directory(contentPath);
+    std::filesystem::create_directory(contentPath / "Images");
 }
 
 std::filesystem::path Project::getContentPath() {
